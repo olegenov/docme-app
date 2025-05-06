2bb9ad34db3282dbf8796b705e2d973b3e13da43
olegenov <nik_kitaev@mail.ru>
2025-05-06 15:08:35 +0300

    Networking service added

---
+++ docme/docme/Common/Managers/NetworkingService/NetworkingService.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+protocol NetworkingService {
+  func get<T: Codable>(path: String, completion: @escaping (Result<T, Error>) -> Void) async
+  func post<T: Request, U: Response>(path: String, requestObject: T, completion: @escaping (Result<U, Error>) -> Void) async
+  func delete<T: Request, U: Response>(path: String, requestObject: T, completion: @escaping (Result<U, Error>) -> Void) async
+  func put<T: Request, U: Response>(path: String, requestObject: T, completion: @escaping (Result<U, Error>) -> Void) async
+  func patch<T: Request, U: Response>(path: String, requestObject: T, completion: @escaping (Result<U, Error>) -> Void) async
+}
